cf575578a9312322cef48ef3292d66a215ad98bd
DanielaChacon <dchacons96@gmail.com>
2018-05-09 17:13:55 -0300

    avance conjunto miercoles 9

--- inventarioCEI/urls.py
+++ inventarioCEI/urls.py
@@ -7,6 +7,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 
+app_name = 'inventario'
 urlpatterns = [
     path('', views.index, name='index'),
     url(r'^profile/$', views.showProfile, name='profile'),
