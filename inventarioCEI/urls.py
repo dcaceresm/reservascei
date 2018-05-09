efb62165de25138d9154212cf2d5fed7651725ac
DanielaChacon <dchacons96@gmail.com>
2018-05-09 17:13:55 -0300

    avance conjunto miercoles 9

--- inventarioCEI/urls.py
+++ inventarioCEI/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'inventario'
 urlpatterns = [
     path('', views.index, name='index'),
     path('landingAdmin/', views.LandingAdmin.as_view(), name='landingAdmin'),
--- inventarioCEI/views.py
+++ inventarioCEI/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-
+from .models import *
+from django.shortcuts import render, redirect, get_object_or_404
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -9,6 +10,18 @@ def index(request):
 class LandingAdmin(TemplateView):
     template_name = "landingPageAdmin.html"
 
-    def get_context_data(self, **kwargs):
-        return {}
 
+    def get_context_data(self, **kwargs):
+        context = super(LandingAdmin, self).get_context_data(**kwargs)
+        context['reservas'] = Reserva.objects.all().order_by("-pk")
+        prestamos = Prestamo.objects.all().order_by("-pk")
+        articulos = Articulo.objects.all().order_by("-pk")
+        espacios = Espacio.objects.all().order_by("-pk")
+        return context
+
+def AceptarReservas(request):
+    id_elemento = request.POST["fila"]
+    reserva = get_object_or_404(Reserva, pk=id_elemento)
+    reserva.estado_reserva = "Aceptado"
+    reserva.save()
+    return redirect('inventario:landingAdmin')
