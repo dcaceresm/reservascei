f1b885cc6bca1305230a4fdf02c73a006b280d2a
Javier Gomez <javier.gomez.p@ug.uchile.cl>
2018-05-09 17:08:35 -0300

    Added 'Ficha articulo'
    (without css)

--- inventarioCEI/urls.py
+++ inventarioCEI/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-
+from django.urls import path, re_path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('landingAdmin/', views.LandingAdmin.as_view(), name='landingAdmin'),
+    re_path(r'ficha/(?P<id>[0-9]*)/$', views.ficha, name='ficha'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
