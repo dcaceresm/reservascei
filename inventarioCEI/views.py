630b86d9abc2bf12c3913f0d55906c7a309ed9b4
DanielaCampos <daniela.campos.f@ing.uchile.cl>
2018-06-21 13:04:45 -0300

    stable version for merge

--- inventarioCEI/views.py
+++ inventarioCEI/views.py
@@ -7,10 +7,6 @@ from django.views.generic import TemplateView
 from .models import *
 import json
 
-
-# variables globales
-
-
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
