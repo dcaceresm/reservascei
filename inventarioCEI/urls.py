762b60e0d2ef995d2327a106ed230b9ce406e6c2
DanielaCampos <daniela.campos.f@ing.uchile.cl>
2018-06-21 00:14:58 -0300

    Calendar receives events with content type espacio

--- inventarioCEI/urls.py
+++ inventarioCEI/urls.py
@@ -6,13 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('landingUser/', views.buscar, name = 'buscar'),
     path('calendar/', views.calendar, name='calendar'),
-    path('calendar/', views.calendar, name='calendar'),
     path('goToArticulos/', views.goToArticulos, name='goToArticulos'),
     path('goToEspacios/', views.goToEspacios, name='goToEspacios'),
-    path('anteriorSemana/', views.anteriorSemana, name='anteriorSemana'),
-    path('anteriorMes/', views.anteriorMes, name='anteriorMes'),
-    path('siguienteSemana/', views.siguienteSemana, name='siguienteSemana'),
-    path('siguienteMes/', views.siguienteMes, name='siguienteMes'),
     path('busquedaAvanzada/', views.busquedaAvanzada, name='busquedaAvanzada'),
     path('landingAdmin/', views.LandingAdmin.as_view(), name='landingAdmin'),
 ]
